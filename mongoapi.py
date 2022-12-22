624cd54b2bf54034693a1db4b44a63c50b00dccd
Michael Free <michael.d.free@gmail.com>
2022-12-22 13:43:32 -0500

    used black to reformat the code, seems to be better now.  still linting 10/10

--- mongoapi.py
+++ mongoapi.py
@@ -7,31 +7,41 @@ from fastapi import FastAPI, status, HTTPException
 
 ######### JSON SCHEMAS #########
 class ItemIn(BaseModel):
-    """ Item In JSON Schema """
+    """Item In JSON Schema"""
+
     name: str
     price: int
 
+
 class ItemOut(BaseModel):
-    """ Item Out JSON Schema """
+    """Item Out JSON Schema"""
+
     status: int
     message: str
     docid: str
     docdata: dict
 
+
 class ItemOutAll(BaseModel):
-    """ Item Out JSON Schema """
+    """Item Out JSON Schema"""
+
     status: int
     message: str
     docdata: list
 
+
 class ItemUpdate(BaseModel):
-    """ Item Update JSON Schema """
+    """Item Update JSON Schema"""
+
     inputdoc: ItemIn
     outputdoc: ItemIn
 
+
 class HTTPError(BaseModel):
-    """ HTTP Error JSON Schema"""
+    """HTTP Error JSON Schema"""
+
     detail: str
+
 
 ####### FASTAPI SETTINGS #######
 app = FastAPI(
@@ -39,11 +49,11 @@ app = FastAPI(
     description="Project Description",
     openapi_tags=[
         {
-            "name":"Inventory",
+            "name": "Inventory",
             "description": "These are items that are in your collection.",
-            },
+        },
     ],
-    )
+)
 
 ####### MONGODB SETTINGS #######
 mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -59,9 +69,9 @@ inventory = mongodb["inventory"]
     responses={
         status.HTTP_409_CONFLICT: {"model": HTTPError},
         status.HTTP_422_UNPROCESSABLE_ENTITY: {"model": HTTPError},
-        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError}
-        }
-    )
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError},
+    },
+)
 async def create_inventory(create_json: ItemIn):
     """
     Create a new item in the Inventory Collection.
@@ -71,8 +81,8 @@ async def create_inventory(create_json: ItemIn):
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     if find_doc is None:
         try:
@@ -80,32 +90,34 @@ async def create_inventory(create_json: ItemIn):
             return ItemOut(
                 status=status.HTTP_201_CREATED,
                 message="Item created in inventory",
-                docid = str(create_doc.inserted_id),
-                docdata = create_json
+                docid=str(create_doc.inserted_id),
+                docdata=create_json,
             )
 
         except PyMongoError as mongo_error:
             raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=mongo_error
-                ) from mongo_error
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+            ) from mongo_error
 
     if find_doc is not None:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail="Item already exists in Inventory")
+            detail="Item already exists in Inventory",
+        )
+
 
 ####### GET SINGLE DOC #########
-@app.get("/inventory/getone/",
+@app.get(
+    "/inventory/getone/",
     tags=["Inventory"],
     response_model=ItemOut,
     status_code=status.HTTP_200_OK,
     responses={
         status.HTTP_404_NOT_FOUND: {"model": HTTPError},
         status.HTTP_422_UNPROCESSABLE_ENTITY: {"model": HTTPError},
-        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError}
-        }
-    )
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError},
+    },
+)
 async def read_inventory(find_json):
     """
     Retrieve a single item from the Inventory Collection.
@@ -121,29 +133,30 @@ async def read_inventory(find_json):
 
     except ValueError as json_error:
         raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=json_error) from json_error
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=json_error
+        ) from json_error
 
     try:
         ItemIn.parse_obj(validate_json)
 
     except ValidationError as json_error:
         raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=json_error) from json_error
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=json_error
+        ) from json_error
 
     try:
         find_doc = inventory.find_one(validate_json)
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     if find_doc is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Item doesn't exist in Inventory.")
+            detail="Item doesn't exist in Inventory.",
+        )
 
     if find_doc is not None:
         find_docid = str(find_doc["_id"])
@@ -152,27 +165,27 @@ async def read_inventory(find_json):
             status=status.HTTP_200_OK,
             message="Found in Inventory.",
             docid=find_docid,
-            docdata=find_doc
+            docdata=find_doc,
         )
 
+
 ######### GET ALL DOCS #########
-@app.get("/inventory/getall/",
+@app.get(
+    "/inventory/getall/",
     tags=["Inventory"],
     response_model=ItemOutAll,
     status_code=status.HTTP_200_OK,
-    responses={
-        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError}
-        }
-    )
+    responses={status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError}},
+)
 async def all_inventory():
-    """ Get all items in inventory collection. """
+    """Get all items in inventory collection."""
     try:
-        all_docs = inventory.find({}, {"_id":0})
+        all_docs = inventory.find({}, {"_id": 0})
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     doc_list = []
 
@@ -180,25 +193,25 @@ async def all_inventory():
         doc_list.append(docs)
 
     return ItemOutAll(
-        status=status.HTTP_200_OK,
-        message="All items in inventory.",
-        docdata=doc_list
+        status=status.HTTP_200_OK, message="All items in inventory.", docdata=doc_list
     )
 
+
 ####### SEARCH ALL KEYS ########
-@app.get("/inventory/search/{search_query}",
+@app.get(
+    "/inventory/search/{search_query}",
     tags=["Inventory"]
     ## updates here
-    )
+)
 async def search_inventory(search_query):
-    """ Search an entire collection in each key """
+    """Search an entire collection in each key"""
     try:
-        all_docs = inventory.find({}, {"_id":0})
+        all_docs = inventory.find({}, {"_id": 0})
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     search_results = []
 
@@ -210,22 +223,24 @@ async def search_inventory(search_query):
     return ItemOutAll(
         status=status.HTTP_200_OK,
         message="All items in inventory.",
-        docdata=search_results
+        docdata=search_results,
     )
 
+
 ###### UPDATE SINGLE DOC #######
-@app.put("/inventory/update/",
+@app.put(
+    "/inventory/update/",
     tags=["Inventory"],
     response_model=ItemOut,
     status_code=status.HTTP_200_OK,
     responses={
         status.HTTP_404_NOT_FOUND: {"model": HTTPError},
         status.HTTP_422_UNPROCESSABLE_ENTITY: {"model": HTTPError},
-        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError}
-        }
-    )
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError},
+    },
+)
 async def update_inventory(update_json: ItemUpdate):
-    """ Update a document in collection """
+    """Update a document in collection"""
     update_input = update_json.dict()["inputdoc"]
     update_output = update_json.dict()["outputdoc"]
 
@@ -235,18 +250,20 @@ async def update_inventory(update_json: ItemUpdate):
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     if find_input is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Item doesn't exist in Inventory.")
+            detail="Item doesn't exist in Inventory.",
+        )
 
     if find_output is not None:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="Update info already exists in Inventory.")
+            detail="Update info already exists in Inventory.",
+        )
 
     try:
         update_query = {"$set": update_output}
@@ -258,27 +275,29 @@ async def update_inventory(update_json: ItemUpdate):
             status=status.HTTP_200_OK,
             message="Item Updated",
             docid=updated_docid,
-            docdata=updated_item
+            docdata=updated_item,
         )
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
+
 
 ###### DELETE SINGLE DOC #######
-@app.delete("/inventory/delete/",
+@app.delete(
+    "/inventory/delete/",
     tags=["Inventory"],
     response_model=ItemOut,
     status_code=status.HTTP_200_OK,
     responses={
         status.HTTP_404_NOT_FOUND: {"model": HTTPError},
         status.HTTP_422_UNPROCESSABLE_ENTITY: {"model": HTTPError},
-        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError}
-        }
-    )
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {"model": HTTPError},
+    },
+)
 async def delete_inventory(delete_json: ItemIn):
-    """ Delete a document in inventory collection """
+    """Delete a document in inventory collection"""
     delete_input = delete_json.dict()
 
     try:
@@ -287,13 +306,14 @@ async def delete_inventory(delete_json: ItemIn):
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     if find_doc is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="Item doesn't exist in Inventory.")
+            detail="Item doesn't exist in Inventory.",
+        )
 
     try:
         inventory.delete_one(delete_input)
@@ -301,18 +321,18 @@ async def delete_inventory(delete_json: ItemIn):
 
     except PyMongoError as mongo_error:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=mongo_error) from mongo_error
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=mongo_error
+        ) from mongo_error
 
     if check_delete is not None:
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="DB Error")
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="DB Error"
+        )
 
     if check_delete is None:
         return ItemOut(
             status=status.HTTP_200_OK,
             message="Item Deleted",
             docid=str(find_id),
-            docdata=delete_input
+            docdata=delete_input,
         )
